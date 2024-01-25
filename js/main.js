04490b0393cbf3ebaee4beae1f11a4bdf1005fab
AndreyChernetsov <andcher24@gmail.com>
2024-01-26 02:07:07 +0700

    Добавление коментариев

--- js/main.js
+++ js/main.js
@@ -1,5 +1,5 @@
-Vue.component('kanban-card', {
-    props: ['card', 'columnIndex', 'cardIndex'],
+Vue.component('kanban-card', { //Представляет карточку на доске канбана
+    props: ['card', 'columnIndex', 'cardIndex'], //Для получения данных от родительского компонента
     template: `
         <div class="card"
             :class="{ 'completed-on-time': card.status === 'Completed on time', 'overdue': card.status === 'Overdue' }">
@@ -33,53 +33,54 @@ Vue.component('kanban-card', {
     `,
     data() {
         return {
-            showEditForm: false,
-            editedTitle: this.card.title,
-            editedDescription: this.card.description,
-            editedDeadline: this.card.deadline,
-            editedRepeatReason: this.card.repeatReason,
+            showEditForm: false, //используется для отслеживания того, нужно ли отображать форму редактирования
+            editedTitle: this.card.title, //Заголовок
+            editedDescription: this.card.description, //Описание
+            editedDeadline: this.card.deadline, //срок
+            editedRepeatReason: this.card.repeatReason, //причина возврата
         };
     },
-    mounted() {
-        console.log("repeatReason:", this.card.repeatReason, "columnIndex:", this.columnIndex);
+    mounted() { //Метод который будет вызван когда компонент будет успешно вставлен в DOM
+        console.log("repeatReason:", this.card.repeatReason, "columnIndex:", this.columnIndex); //Проверка
     },
-    methods: {
-        editCard() {
+    methods: { //Для редактирования, сохранения и изменений, отмены изменений, удалений, перемещений карточек
+        editCard() { //Для редактирования карточки
             this.showEditForm = true;
         },
-        saveEdits() {
+        saveEdits() { //сохранение изменений
             this.card.title = this.editedTitle;
             this.card.description = this.editedDescription;
             this.card.deadline = this.editedDeadline;
             this.card.repeatReason = this.editedRepeatReason;
             this.card.lastEdited = new Date().toLocaleString();
-            this.showEditForm = false;
+            this.showEditForm = false; //Скрывает форму редактирования
 
-            this.$emit('save-edits');
+            this.$emit('save-edits'); //Реагирует на сохранение
         },
-        cancelEdits() {
+        //События для общения с родилельским компонентом
+        cancelEdits() { //Отмена редактирования
             this.showEditForm = false;
         },
-        deleteCard() {
-            this.$emit('delete-card', this.columnIndex, this.cardIndex);
+        deleteCard() { //Удаление карточки
+            this.$emit('delete-card', this.columnIndex, this.cardIndex); //представляет индексы колонки и карточки, которую нужно удалить.
         },
-        moveToInProgress() {
+        moveToInProgress() { //вызывается при перемещении карточки в колонку Задачи в работе
             this.$emit('move-to-in-progress', this.card, this.columnIndex, this.cardIndex);
         },
-        moveToTesting() {
+        moveToTesting() { //вызывается при перемещении карточки в колонку Тестирование
             this.$emit('move-to-testing', this.card, this.columnIndex, this.cardIndex);
         },
-        moveToDone() {
+        moveToDone() { //вызывается при перемещении карточки в колонку Выполненные задачи
             this.$emit('move-to-done', this.card, this.columnIndex, this.cardIndex);
         },
         returnToInProgress() {
-            const inProgressIndex = 1;
+            const inProgressIndex = 1; //Индекс колонки
 
-            this.$emit('save-edits', {
+            this.$emit('save-edits', { //Сохранение
                 repeatReason: this.editedRepeatReason,
             });
 
-            this.$parent.columns[inProgressIndex].cards.push({
+            this.$parent.columns[inProgressIndex].cards.push({ //новый объект карточки который содержит обновленные данные
                 title: this.card.title,
                 description: this.card.description,
                 deadline: this.card.deadline,
@@ -91,18 +92,18 @@ Vue.component('kanban-card', {
             this.$parent.columns[this.columnIndex].cards.splice(this.cardIndex, 1);
             this.showEditForm = false;
         },
-        moveToCompletedWithDeadlineCheck() {
-            const completedIndex = 3;
-            const deadline = new Date(this.card.deadline);
-            const currentDate = new Date();
+        moveToCompletedWithDeadlineCheck() { //Перемещение в выполненные задачи
+            const completedIndex = 3; //Индекс колонки
+            const deadline = new Date(this.card.deadline); //объект на основе дедлайна
+            const currentDate = new Date(); //Текущая дата
 
-            if (currentDate > deadline) {
+            if (currentDate > deadline) { //Проверка закончено ли вовремя
                 this.card.status = 'Опоздание';
             } else {
                 this.card.status = 'Закончено во время';
             }
 
-            this.$parent.columns[completedIndex].cards.push({
+            this.$parent.columns[completedIndex].cards.push({ //новый объект карточки который содержит обновленные данные
                 title: this.card.title,
                 description: this.card.description,
                 deadline: this.card.deadline,
@@ -112,58 +113,58 @@ Vue.component('kanban-card', {
                 repeatReason: this.card.repeatReason,
             });
 
-            this.$parent.columns[this.columnIndex].cards.splice(this.cardIndex, 1);
+            this.$parent.columns[this.columnIndex].cards.splice(this.cardIndex, 1); //Процесс удаления карточки
         },
     },
 });
 
-new Vue({
+new Vue({ //экзеипляр для всего приложения
     el: '#app',
-    data: {
+    data: { //Данные приложения
         columns: [
             { name: 'Запланированные задачи', cards: [] },
             { name: 'Задачи в работе', cards: [] },
             { name: 'Тестирование', cards: [] },
             { name: 'Выполненные задачи', cards: [] }
         ],
-        newCard: { title: '', description: '', deadline: '' }
+        newCard: { title: '', description: '', deadline: '' } //Для создания новых карточек
     },
     computed: {
-        isFormValid() {
+        isFormValid() { //используется для проверки валидности данных перед добавлением новой карточки
             return this.newCard.title && this.newCard.description && this.newCard.deadline;
         },
-        canDelete() {
+        canDelete() { //может ли пользователь удалять карточки из текущей колонки
             return this.columnIndex === 0;
           },
     },
-    methods: {
-        addCard(columnIndex) {
-            if (columnIndex === 0 && this.isFormValid) {
+    methods: { //Функции для добавления, удаления и перемещения карточек между колонками
+        addCard(columnIndex) { //Добавляет новую карточку
+            if (columnIndex === 0 && this.isFormValid) { //Проверка колонки и валидности данных
                 const newCard = {
                     title: this.newCard.title,
                     description: this.newCard.description,
                     deadline: this.newCard.deadline,
-                    dateCreated: new Date().toLocaleString(),
-                    lastEdited: new Date().toLocaleString()
+                    dateCreated: new Date().toLocaleString(), //Дата и время создания карточки, с помощью new Date().toLocaleString()
+                    lastEdited: new Date().toLocaleString() //Последнее редактирование
                 };
-                this.columns[columnIndex].cards.push(newCard);
-                this.clearNewCard();
+                this.columns[columnIndex].cards.push(newCard); //Добавляется в массив соответствующей колонки
+                this.clearNewCard(); //Обнуляет форму создания
             }
         },
-        clearNewCard() {
+        clearNewCard() { //обнуляет данные новой карточки
             this.newCard = { title: '', description: '', deadline: '' };
         },
-        deleteCard(columnIndex, cardIndex) {
-            if (columnIndex >= 0 && columnIndex < this.columns.length) {
-                if (cardIndex >= 0 && cardIndex < this.columns[columnIndex].cards.length) {
+        deleteCard(columnIndex, cardIndex) { //Удаление карточки из массива
+            if (columnIndex >= 0 && columnIndex < this.columns.length) { //В пределах индексов колонок
+                if (cardIndex >= 0 && cardIndex < this.columns[columnIndex].cards.length) { //находится в пределах допустимого диапазона индексов карточек в текущей колонке
                     this.columns[columnIndex].cards.splice(cardIndex, 1);
                 }
             }
         },
-        moveToInProgress(originalCard, columnIndex, cardIndex) {
+        moveToInProgress(originalCard, columnIndex, cardIndex) { //перемещает карточку из текущей колонки в колонку Задачи в работе
             const inProgressIndex = 1;
 
-            this.columns[inProgressIndex].cards.push({
+            this.columns[inProgressIndex].cards.push({ //Создается новый объект карточки в колонке Задачи в работе используя данные из оригинальной карточки
                 title: originalCard.title,
                 description: originalCard.description,
                 deadline: originalCard.deadline,
@@ -171,12 +172,12 @@ new Vue({
                 lastEdited: originalCard.lastEdited
             });
 
-            this.columns[columnIndex].cards.splice(cardIndex, 1);
+            this.columns[columnIndex].cards.splice(cardIndex, 1); //Удаляется из текущей колонки
         },
-        moveToTesting(originalCard, columnIndex, cardIndex) {
+        moveToTesting(originalCard, columnIndex, cardIndex) { //перемещает карточку из текущей колонки в колонку Тестирование
             const testingIndex = 2;
 
-            this.columns[testingIndex].cards.push({
+            this.columns[testingIndex].cards.push({ //Создается новый объект карточки в колонке Тестирование используя данные из оригинальной карточки
                 title: originalCard.title,
                 description: originalCard.description,
                 deadline: originalCard.deadline,
@@ -185,12 +186,12 @@ new Vue({
                 repeatReason: originalCard.repeatReason,
             });
 
-            this.columns[columnIndex].cards.splice(cardIndex, 1);
+            this.columns[columnIndex].cards.splice(cardIndex, 1); //Удаляется из текущей колонки
         },
-        moveToDone(originalCard, columnIndex, cardIndex) {
+        moveToDone(originalCard, columnIndex, cardIndex) { //перемещение карточки из текущей колонки в колонку Выполненные задачи
             const doneIndex = 3;
 
-            this.columns[doneIndex].cards.push({
+            this.columns[doneIndex].cards.push({ //Создается новый объект карточки в колонке Выполненные задачи используя данные из оригинальной карточки
                 title: originalCard.title,
                 description: originalCard.description,
                 deadline: originalCard.deadline,
@@ -198,47 +199,21 @@ new Vue({
                 lastEdited: originalCard.lastEdited
             });
 
-            this.columns[columnIndex].cards.splice(cardIndex, 1);
+            this.columns[columnIndex].cards.splice(cardIndex, 1); //Удаляется из текущей колонки
         },
-        returnToInProgress(originalCard, columnIndex, cardIndex) {
-            const inProgressIndex = 1;
-
-
-            if (repeatReasonInCompleted) {
-                const isCardAlreadyInInProgress = this.$parent.columns[inProgressIndex].cards.some(card => card.title === this.card.title);
-    
-                if (!isCardAlreadyInInProgress) {
-                    this.$parent.columns[inProgressIndex].cards.push({
-                        title: this.card.title,
-                        description: this.card.description,
-                        deadline: this.card.deadline,
-                        dateCreated: this.card.dateCreated,
-                        lastEdited: new Date().toLocaleString(),
-                        repeatReason: this.card.repeatReason,
-                    });
-    
-                    this.$parent.columns[this.columnIndex].cards.splice(this.cardIndex, 1);
-                    this.showEditForm = false;
-                } else {
-                    alert("Карточка уже находится в столбце 'Задачи в работе'");
-                }
-            } else {
-                alert("Нельзя вернуть задачу без указания 'Причина повторения'");
-            }
-        },
-        moveToCompletedWithDeadlineCheck(originalCard, columnIndex, cardIndex) {
+        moveToCompletedWithDeadlineCheck(originalCard, columnIndex, cardIndex) {  //для перемещения карточки из текущей колонки в колонку Выполненные задачи с проверкой наличия опоздания
             const completedIndex = 3;
 
-            const deadline = new Date(originalCard.deadline);
-            const currentDate = new Date();
+            const deadline = new Date(originalCard.deadline); //Создается объект даты deadline на основе дедлайна из оригинальной карточки
+            const currentDate = new Date(); //Создается объект текущей даты
 
-            if (currentDate > deadline) {
+            if (currentDate > deadline) { //Выполняется проверка просрочена ли дата дедлайна
                 originalCard.status = 'С опозданием';
             } else {
                 originalCard.status = 'Закончено во время';
             }
 
-            const cardToMove = {
+            const cardToMove = { //Создается объект на основе данных из оригинальной карточки и установленный статус.
                 title: originalCard.title,
                 description: originalCard.description,
                 deadline: originalCard.deadline,
@@ -248,9 +223,9 @@ new Vue({
                 repeatReason: originalCard.repeatReason,
             };
         
-            this.$parent.columns[completedIndex].cards.push(cardToMove);
+            this.$parent.columns[completedIndex].cards.push(cardToMove); //Карточка добавляется в колонку Выполненные задачи
         
-            this.$parent.columns[columnIndex].cards.splice(cardIndex, 1);
+            this.$parent.columns[columnIndex].cards.splice(cardIndex, 1); // удаляется из текущей колонки
         }
     }
 });
